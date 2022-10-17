530e6432b0537bf24b9bc5daca87298d799c2795
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-10-17 13:58:47 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const board = document.querySelector("#board");
+const SQUARES_NUMBER = 500;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => {
+    setColor(square);
+  });
+
+  square.addEventListener("mouseleave", () => {
+    removeColor(square);
+  });
+
+  board.append(square);
+}
+
+function setColor(el) {
+  const color = getRandomColor();
+  el.style.backgroundColor = color;
+  el.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}, 0 0 25px ${color}`;
+}
+
+function removeColor(el) {
+  el.style.backgroundColor = "#1d1d1d";
+  el.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * letters.length)];
+  }
+  return color;
+}
